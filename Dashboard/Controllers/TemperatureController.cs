65eed433c75e6992f843a2d05db3b32b00348d06
unknown <jstur@jstur-sp3win10.northamerica.corp.microsoft.com>
2015-08-25 12:07:29 -0400

    updated temprature display to show more than one device

--- Dashboard/Controllers/TemperatureController.cs
+++ Dashboard/Controllers/TemperatureController.cs
@@ -1,17 +1,39 @@
-﻿using System;
+﻿using Dashboard.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using WoodStove.Core;
 
 namespace Dashboard.Controllers
 {
+    [Authorize]
     public class TemperatureController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+
         // GET: Temperature
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            var dvm = new DevicesViewModel();
+
+            dvm.Devices = await GetUserDevices().ToListAsync();
+
+            return View(dvm);
+        }
+
+        private IQueryable<Device> GetUserDevices()
+        {
+            var userId = this.User.Identity.GetUserId();
+            IQueryable<Device> devices = this.db.Users.Include(u => u.Devices).Where(u => u.Id == userId).SelectMany(u => u.Devices);
+            return devices;
         }
     }
+
+   
 }
---
+++ Dashboard/Models/DevicesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WoodStove.Core;
+
+namespace Dashboard.Models
+{
+    public class DevicesViewModel
+    {
+        public List<Device> Devices { get; set; }
+    }
+}
